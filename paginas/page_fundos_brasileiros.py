d7d1abf1da90ec79ff691b10f51faf40c4afe594
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2023-09-16 14:50:59 -0300

    Ajustes... validaçõe....

--- paginas/page_fundos_brasileiros.py
+++ paginas/page_fundos_brasileiros.py
@@ -6,19 +6,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from datetime import datetime, timedelta
+
+
+def mes_ano_no_passado(mes, ano):
+    # Obtém o mês e o ano atuais
+    data_atual = datetime.now()
+    ano_atual = data_atual.year
+    mes_atual = data_atual.month
+
+    # Compara o ano e o mês fornecidos com o ano e o mês atuais
+    if int(ano) < ano_atual or (int(ano) == ano_atual and int(mes) < mes_atual):
+        return True
+    else:
+        return False
+
+
+def data_anterior(data):
+    # Converte a string de data para um objeto datetime
+    data_obj = datetime.strptime(data, "%Y-%m-%d")
+
+    # Calcula a data anterior subtraindo um dia
+    data_anterior = data_obj - timedelta(days=1)
+
+    # Formata a data anterior de volta para o formato 'YYYY-MM-DD'
+    data_anterior_formatada = data_anterior.strftime("%Y-%m-%d")
+
+    return data_anterior_formatada
+
 
 @st.cache_data
-def carregar_dados_cadastrais():
+def carregar_todos_dados_cadastrais():
     url = "http://dados.cvm.gov.br/dados/FI/CAD/DADOS/cad_fi.csv"
     cadastral = pd.read_csv(url, sep=";", encoding="ISO-8859-1")
     return cadastral
 
 
-@st.cache_data
-def carregar_dados_cadastrais_de(_cnpjs: list):
-    cadastral = carregar_dados_cadastrais()
+def carregar_dados_cadastrais_de(cnpjs: list):
+    cadastral = carregar_todos_dados_cadastrais()
     cadastral = cadastral[~cadastral["SIT"].str.contains("CANCELADA")]
-    return cadastral[cadastral["CNPJ_FUNDO"].isin(_cnpjs)]
+    return cadastral[cadastral["CNPJ_FUNDO"].isin(cnpjs)]
 
 
 @st.cache_data
@@ -91,16 +118,24 @@ def main():
         quantidade = st.text_input("Quantidade", "10")
 
     if st.button("Carregar dados..."):
-        tab1, tab2, tab3 = st.tabs(["Maior PL", "Mais subiram", "Mais caíram"])
+        if not mes_ano_no_passado(mes, ano):
+            mensagens.error("Informe um mês ano que já tenha encerrado", icon="🚨")
+            st.stop()
+
         informes_diarios = carregar_dados_informes_diarios(mes, ano)
+        maior_data_dos_dados = informes_diarios["DT_COMPTC"].max()
+        # maior_data_dos_dados = data_anterior(maior_data_dos_dados)
+
+        st.write(f"Considerados dados até {maior_data_dos_dados}")
+        tab1, tab2, tab3 = st.tabs(["Maior PL", "Mais subiram", "Mais caíram"])
 
         with tab1:
             st.write(
                 f"### :statue_of_liberty: {quantidade} maiores fundos (considerando PL) no período selecionado ({mes}/{ano})"
             )
             comparativo = informes_diarios[
-                informes_diarios["DT_COMPTC"] == f"{ano}-{mes}-31"
-            ]
+                informes_diarios["DT_COMPTC"] == maior_data_dos_dados
+            ].copy()
             comparativo.sort_values("VL_PATRIM_LIQ", inplace=True)
             cnpjs = comparativo.CNPJ_FUNDO.iloc[-int(quantidade) :]
             # fundos = informes_diarios[informes_diarios["CNPJ_FUNDO"].isin(cnpjs)]
@@ -109,7 +144,25 @@ def main():
             retorno = carregar_dados_cadastrais_de(cnpjs.values)
             retorno.sort_values("VL_PATRIM_LIQ", inplace=True, ascending=False)
             retorno = retorno.head(int(quantidade))
-            st.write(retorno)
+            st.write(
+                retorno[
+                    [
+                        "VL_PATRIM_LIQ",
+                        "TP_FUNDO",
+                        "CNPJ_FUNDO",
+                        "DENOM_SOCIAL",
+                        "SIT",
+                        "CLASSE",
+                        "FUNDO_EXCLUSIVO",
+                        "PUBLICO_ALVO",
+                        "DIRETOR",
+                        "GESTOR",
+                        "AUDITOR",
+                        "CUSTODIANTE",
+                        "CLASSE_ANBIMA",
+                    ]
+                ]
+            )
             # st.pyplot(variacao)
         with tab2:
             st.write(
@@ -117,16 +170,19 @@ def main():
             )
             cotas_normalizadas = carregar_cotas_normalizadas(mes, ano)
             altas = (
-                cotas_normalizadas.sort_values(f"{ano}-{mes}-31", ascending=False)[
+                cotas_normalizadas.sort_values(maior_data_dos_dados, ascending=False)[
                     : int(quantidade)
-                ][f"{ano}-{mes}-31"]
+                ][maior_data_dos_dados]
                 - 1
             ) * 100
+            # st.write(altas)
             fundo_df_alta = pd.DataFrame(
                 columns=["Retornos", "Nome do Fundo", "Classe", "Patrimônio Líquido"]
             )
+            # st.write(fundo_df_alta)
             for cnpj in altas.index:
                 fundo = carregar_dados_cadastrais_de([cnpj])
+                # st.write(fundo)
                 fundo_df_alta.loc[cnpj] = [
                     altas[altas.index == cnpj].values[0],
                     fundo["DENOM_SOCIAL"].values[0],
@@ -148,9 +204,9 @@ def main():
             )
             cotas_normalizadas = carregar_cotas_normalizadas(mes, ano)
             baixas = (
-                cotas_normalizadas.sort_values(f"{ano}-{mes}-31", ascending=True)[
+                cotas_normalizadas.sort_values(maior_data_dos_dados, ascending=True)[
                     : int(quantidade)
-                ][f"{ano}-{mes}-31"]
+                ][maior_data_dos_dados]
                 - 1
             ) * 100
             fundo_df_baixa = pd.DataFrame(
