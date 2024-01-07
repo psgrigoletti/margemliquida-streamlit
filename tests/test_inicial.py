c543d16946dd2519766dc909f7a16eb74fe83c1f
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2024-01-07 01:01:12 -0300

    Adicionando um teste

--- tests/test_inicial.py
+++ tests/test_inicial.py
@@ -1,5 +1,14 @@
-import pytest
+"""Testes do módulo utils"""
+from src.utils.data_hora_utils import DataHoraUtils
 
 
 def test_inicial():
-    assert "teste".upper() == "TESTE"
+    """Teste inicial"""
+    assert "teste".upper() == "TESTE", "Teste sempre correto"
+
+
+def test_retorna_data_atual_formato_ddmmyyyy():
+    """Teste para classe utilitária DataHoraUtils"""
+    assert len(DataHoraUtils.retorna_data_atual_formato_ddmmyyyy()) == 10
+    assert DataHoraUtils.retorna_data_atual_formato_ddmmyyyy() is not None
+
